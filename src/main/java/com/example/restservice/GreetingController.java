195ec93651a88599027f348c81223e855dee5e0d
Kei Omizo <kay.akubichan.dev@gmail.com>
2022-08-09 10:31:43 +0900

    update

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	private static Logger logger = Logger.getLogger(GreetingController.class.getName());
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello, secretname %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/api/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		String val = getSecret(name);
+		return new Greeting(counter.incrementAndGet(), String.format(template, val));
 	}
+
+	private String getSecret(String name) {
+		String val = null;
+		if (name != null && name.length() > 0) {
+			val = System.getenv(name);
+			if (val == null) val = "ENV NOT FOUND";
+		}
+		else {
+			val = "NUL";
+		}
+		logger.info("##########" + name + "," + val + "##########");
+		return val;
+	}
+
 }
